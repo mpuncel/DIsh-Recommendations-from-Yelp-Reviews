5d88593db5470e6af8aa9c122dcdf774ab5c9e4a
Michael Puncel <mpuncel@mit.edu>
2012-11-11 00:25:18 -0500

    fixed snippets

--- experiment.py
+++ experiment.py
@@ -15,14 +15,14 @@ for line in menu:
 
 max_length = max([len(x.split(' ')) for x in words])
 reviews = file('50_review_restaurants/Amelias_Trattoria.json', 'r')
-print words
 for line in reviews:
 	obj = json.loads(line)
 	if obj['type'] == 'review':
 		#print obj['text']
 		text = [x.lower() for x in obj['text'].split(' ')]
-		for i in xrange(0, len(text)):
-			for j in xrange(0, max_length):
+		for i in range(0, len(text)):
+			for j in reversed(xrange(0, max_length)):
 				excerpt = text[i : rightclip(i + j + 1, len(text))]
 				if ' '.join(excerpt) in words:
 					print ' '.join(text[leftclip(i - 5): rightclip(i + j + 6, len(text))])
+					break
