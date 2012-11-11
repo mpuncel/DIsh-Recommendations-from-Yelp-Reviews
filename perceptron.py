d02891b76481c1d4a7793af13eb4ae7c0a06d234
Michael Puncel <mpuncel@mit.edu>
2012-11-11 14:51:27 -0500

    perceptron

---
+++ perceptron.py
@@ -0,0 +1,155 @@
+import re
+
+alpha = 1
+T = 10
+test_snippets_base = 'test/Snippets/'
+predictions_base = 'test/predictions/'
+training_snippets_base = 'training/Snippets/'
+training_labels_base = 'training/Snippets/training/'
+training_suffix = '_training.txt'
+exclusion_regex = r'[)(\n.?!:,]'
+training_restaurants = [
+	'Al_Forno_Restaurant',
+	'Amelias_Trattoria',
+	'Angelinos_Cafe',
+	'Bacaro_LA',
+	'Buca_Di_Beppo',
+	'DAmicos_Italian_Market_Cafe',
+	'Babbos_Original_Mediterranean_Bistro',
+	'Bettolona',
+	'Buona_Tavola',
+]
+restaurants = [
+	'411_West',
+	'800_Degrees_Neapolitan_Pizzeria',
+	'Al_Forno_Restaurant',
+	'Allegro_Pizza',
+	'Amelias_Trattoria',
+	'Angelinos_Cafe',
+	'Antica_Osteria_Restaurant',
+	'Antico_Pizza',
+	'Aruffos_Italian_Cuisine',
+	'Babbos_Original_Mediterranean_Bistro',
+	'Bacaro',
+	'Bacaro_LA',
+	'Bad_Horse_Pizza',
+	'Baraonda_Italian_Restaurant',
+	'Bertuccis',
+	'Bertuccis_Italian_Restaurant_-_Kendall_Square',
+	'Bettolona',
+	'Buca_di_Beppo',
+	'Buona_Tavola',
+	'Cafe_47',
+	'Campo',
+	'Celestino',
+	'Che_Bella_Pizza',
+	'Cinderellas_Restaurant',
+	'DAmicos_Italian_Market_Cafe',
+	'DAmores_Pizza',
+	'DeFazios_Pizzeria',
+	'Dolce_Cafe_&_Bakery',
+	'Dominicks',
+	'Eno_Terra',
+	'Euro_Cafe',
+	'Girasole_Restaurant',
+	'Gratzi',
+	'Gypsys_Trattoria_Italiano',
+	'Italian_Express',
+	'La_Parolaccia_Osteria_Italiana',
+	'La_Piccoletta',
+	'Ledo_Restaurant',
+	'Lido',
+	'Little_Azio',
+	'Mama_Palmas_Gourmet_Pizza',
+	'Mani_Osteria_&_Bar',
+	'Margarita_Pizza_Bar',
+	'Max_Cafe',
+	'Max_Soha',
+	'Mediterra_Restaurant',
+	'Milanese_Caffe',
+	'Pacci_Ristorante',
+	'Pagliacci_Pizza',
+	'Palazzo_Giuseppes',
+	'Pallino_Pastaria',
+	'Panini_Cafe',
+	'Pasta_Bene',
+	'Pasta_Roma',
+	'Piatti_Ristorante_&_Bar',
+	'Pisticci',
+	'Pizza_House',
+	'Pizza_N_Such',
+	'Prego',
+	'Rialto',
+	'Sagra',
+	'Salento',
+	'Sezz_Medi',
+	'Silvios_Organic_Pizza',
+	'Skylight_Gardens',
+	'Tanino_Restaurant',
+	'Teresa_Caffe',
+	'Tootsies_At_The_Stanford_Barn',
+	'Toscano_&_Sons_Italian_Market',
+	'Trattoria_Neapolis',
+	'Tutti_Mangia_Italian_Grill',
+	'V_&_T_Pizzeria_&_Restaurant',
+	'Veni_Vidi_Vici',
+	'Viztango_Cafe',
+	'Voza',
+]
+
+##build vocabulary
+vocab = set()
+
+for training_restaurant_name in training_restaurants:
+	snips = file(training_snippets_base + training_restaurant_name + '.txt', 'r')
+	for line in snips:
+		vocab = vocab.union(set(re.sub(exclusion_regex, '', line.lower()).split(' ')))
+	snips.close()
+
+vocab = list(vocab)
+weight = [0]*len(vocab)
+
+##get weights
+for i in range(0, T):
+	for training_restaurant_name in training_restaurants:
+		snips = file(training_snippets_base + training_restaurant_name + '.txt', 'r')
+		training = file(training_labels_base + training_restaurant_name + training_suffix, 'r')
+		for line in snips:
+			label = int(training.readline())
+			tokenset = set(re.sub(exclusion_regex, '', line).split(' '))
+
+			vector = [0]*len(vocab)
+			for j in xrange(0, len(vocab)):
+				if vocab[j] in tokenset:
+					vector[j] = 1
+			product = [w * x for w, x in zip(weight, vector)]
+			if sum(product) >= 0:
+				sign = 1
+			else:
+				sign = -1
+			if int(sign) != label:
+				adjustment = [v * alpha * label for v in vector]
+				weight = [w + a for w,a in zip(weight, adjustment)]
+
+tups = zip(weight, vocab)
+tups.sort()
+print tups
+
+##apply to test data
+for restaurant_name in restaurants:
+	try:
+		snips = file(test_snippets_base + restaurant_name + '.txt', 'r')
+		out = file(predictions_base + restaurant_name + '.txt', 'w')
+		for line in snips:
+			tokenset = set(re.sub(exclusion_regex, '', line).split(' '))
+
+			vector = [0]*len(vocab)
+			for j in xrange(0, len(vocab)):
+				if vocab[j] in tokenset:
+					vector[j] = 1
+
+			product = [w * x for w, x in zip(weight, vector)]
+			out.write(str(sum(product)) + '\n')
+	except:
+		continue
+
--- snippet_generator.py
+++ snippet_generator.py
@@ -1,14 +1,84 @@
 import json
-words = []
 menu_base = 'Menus/'
-output_base = 'Snippets/'
+training_output_base = 'training/Snippets/'
+test_output_base = 'test/Snippets/'
 reviews_base = '50_review_restaurants/'
 restaurants = [
+	'411_West',
+	'800_Degrees_Neapolitan_Pizzeria',
 	'Al_Forno_Restaurant',
-	'Angelinos_Cafe',
-	'Bacaro_LA',
-	'Buca_Di_Beppo',
+	'Allegro_Pizza',
 	'Amelias_Trattoria',
+	'Angelinos_Cafe',
+	'Antica_Osteria_Restaurant',
+	'Antico_Pizza',
+	'Aruffos_Italian_Cuisine',
+	'Babbos_Original_Mediterranean_Bistro',
+	'Bacaro',
+	'Bacaro_LA',
+	'Bad_Horse_Pizza',
+	'Baraonda_Italian_Restaurant',
+	'Bertuccis',
+	'Bertuccis_Italian_Restaurant_-_Kendall_Square',
+	'Bettolona',
+	'Buca_di_Beppo',
+	'Buona_Tavola',
+	'Cafe_47',
+	'Campo',
+	'Celestino',
+	'Che_Bella_Pizza',
+	'Cinderellas_Restaurant',
+	'DAmicos_Italian_Market_Cafe',
+	'DAmores_Pizza',
+	'DeFazios_Pizzeria',
+	'Dolce_Cafe_&_Bakery',
+	'Dominicks',
+	'Eno_Terra',
+	'Euro_Cafe',
+	'Girasole_Restaurant',
+	'Gratzi',
+	'Gypsys_Trattoria_Italiano',
+	'Italian_Express',
+	'La_Parolaccia_Osteria_Italiana',
+	'La_Piccoletta',
+	'Ledo_Restaurant',
+	'Lido',
+	'Little_Azio',
+	'Mama_Palmas_Gourmet_Pizza',
+	'Mani_Osteria_&_Bar',
+	'Margarita_Pizza_Bar',
+	'Max_Cafe',
+	'Max_Soha',
+	'Mediterra_Restaurant',
+	'Milanese_Caffe',
+	'Pacci_Ristorante',
+	'Pagliacci_Pizza',
+	'Palazzo_Giuseppes',
+	'Pallino_Pastaria',
+	'Panini_Cafe',
+	'Pasta_Bene',
+	'Pasta_Roma',
+	'Piatti_Ristorante_&_Bar',
+	'Pisticci',
+	'Pizza_House',
+	'Pizza_N_Such',
+	'Prego',
+	'Rialto',
+	'Sagra',
+	'Salento',
+	'Sezz_Medi',
+	'Silvios_Organic_Pizza',
+	'Skylight_Gardens',
+	'Tanino_Restaurant',
+	'Teresa_Caffe',
+	'Tootsies_At_The_Stanford_Barn',
+	'Toscano_&_Sons_Italian_Market',
+	'Trattoria_Neapolis',
+	'Tutti_Mangia_Italian_Grill',
+	'V_&_T_Pizzeria_&_Restaurant',
+	'Veni_Vidi_Vici',
+	'Viztango_Cafe',
+	'Voza',
 ]
 
 NUM_TRAILING_WORDS = 12
@@ -19,33 +89,34 @@ def leftclip(amt):
 	return max(amt, 0)
 
 for restaurant_name in restaurants:
-	menu = file(menu_base + restaurant_name + '.txt', 'r')
-	snippets = file(output_base + restaurant_name + '.txt', 'w')
-	line = menu.readline()
-	for line in menu:
-		words.append(line.strip().split(';')[0].lower())
-		'''
-		for item in line.strip().split(';'):
-			words.append(item.lower())
-		'''
+	try:
+		words = []
+		menu = file(menu_base + restaurant_name + '.txt', 'r')
+		snippets = file(training_output_base + restaurant_name + '.txt', 'w')
+		line = menu.readline()
+		for line in menu:
+			words.append(line.strip().split(';')[0].lower())
 
-	max_length = max([len(x.split(' ')) for x in words])
-	reviews = file(reviews_base + restaurant_name + '.json', 'r')
-	for line in reviews:
-		try:
-			obj = json.loads(line)
-			if obj['type'] == 'review':
-				text = [x.lower() for x in obj['text'].split(' ')]
-				for i in range(0, len(text)):
-					for j in reversed(xrange(0, max_length)):
-						excerpt = text[i : rightclip(i + j + 1, len(text))]
-						if ' '.join(excerpt) in words:
-							string = ' '.join(text[leftclip(i): rightclip(i + j + 1 + NUM_TRAILING_WORDS, len(text))])
-							snippets.write(string if string.find('\n') < 0 else string[0: string.find('\n')])
-							snippets.write('\n')
-							break
-		except:
-			break
-	snippets.close()
-	menu.close()
-	reviews.close()
+		max_length = max([len(x.split(' ')) for x in words])
+		reviews = file(reviews_base + restaurant_name + '.json', 'r')
+		for line in reviews:
+			try:
+				obj = json.loads(line)
+				if obj['type'] == 'review':
+					text = [x.lower() for x in obj['text'].split(' ')]
+					for i in range(0, len(text)):
+						for j in reversed(xrange(0, max_length)):
+							excerpt = text[i : rightclip(i + j + 1, len(text))]
+							if ' '.join(excerpt) in words:
+								string = ' '.join(text[leftclip(i): rightclip(i + j + 1 + NUM_TRAILING_WORDS, len(text))])
+								snippets.write(string if string.find('\n') < 0 else string[0: string.find('\n')])
+								snippets.write('\n')
+								break
+			except:
+				break
+		snippets.close()
+		menu.close()
+		reviews.close()
+	except:
+		continue
+
