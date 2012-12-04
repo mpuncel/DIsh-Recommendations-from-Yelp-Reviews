5568ecf639de3dcfa6a9584a7635dcffedc168eb
Michael Puncel <mpuncel@mit.edu>
2012-12-04 00:56:32 -0500

    shortpredictions

--- perceptron.py
+++ perceptron.py
@@ -4,9 +4,9 @@ import re
 
 alpha = 1
 T = 10
-test_snippets_base = 'test/longSnippets/'
-predictions_base = 'test/longpredictions/'
-training_snippets_base = 'training/longSnippets/'
+test_snippets_base = 'test/shortSnippets/'
+predictions_base = 'test/shortpredictions/'
+training_snippets_base = 'training/shortSnippets/'
 training_suffix = '_training.txt'
 exclusion_regex = r'[)(\n.?!:,]'
 training_restaurants = [
