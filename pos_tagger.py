3cadd6942a10b40df314d4901cf77f40ef2efd76
nkumar7 <nischaykumar@gmail.com>
2012-12-11 22:55:03 -0500

    New Model Stats Figure

--- pos_tagger.py
+++ pos_tagger.py
@@ -5,16 +5,16 @@ import os
 reviews_base = '50_review_restaurants/'
 out_base = '50_review_restaurants_POS_tagged/'
 restaurants = [
-#	'411_West',
-#	'800_Degrees_Neapolitan_Pizzeria',
-#	'Al_Forno_Restaurant',
-#	'Allegro_Pizza',
-#	'Amelias_Trattoria',
-#	'Angelinos_Cafe',
-#	'Antica_Osteria_Restaurant',
-#	'Antico_Pizza',
-#	'Aruffos_Italian_Cuisine',
-#	'Babbos_Original_Mediterranean_Bistro',
+	'411_West',
+	'800_Degrees_Neapolitan_Pizzeria',
+	'Al_Forno_Restaurant',
+	'Allegro_Pizza',
+	'Amelias_Trattoria',
+	'Angelinos_Cafe',
+	'Antica_Osteria_Restaurant',
+	'Antico_Pizza',
+	'Aruffos_Italian_Cuisine',
+	'Babbos_Original_Mediterranean_Bistro',
 	'Bacaro',
 	'Bacaro_LA',
 	'Bad_Horse_Pizza',
@@ -96,6 +96,7 @@ for restaurant_name in restaurants:
                 text=obj['text']
                 tempFile=codecs.open('Temp.txt','w','utf-8')
                 tempFile.write(text)
+                tempFile.close()
                 in_file,out_file,error_file=os.popen3("java -mx300m -cp 'stanford-postagger-full-2012-11-11/stanford-postagger-3.1.4.jar:' edu.stanford.nlp.tagger.maxent.MaxentTagger -model stanford-postagger-full-2012-11-11/models/wsj-0-18-left3words.tagger -textFile Temp.txt")
                 tagFile=file(new_base+restaurant_name+'_review_'+str(count)+'_POS_Tagged.txt','w') 
                 tagFile.write(''.join(out_file))
@@ -103,6 +104,7 @@ for restaurant_name in restaurants:
         except:
             logFile=codecs.open('LogFile.txt','a','utf-8')
             logFile.write(restaurant_name+': Line'+str(count-1)+'\n')
+            logFile.close()
         count+=1
     reviews.close()
     
